efc489a2e45936b347da8b6b81f7f25dedf57d94
cosmosmos <qnaqna7845@naver.com>
2021-09-26 02:40:13 +0900

    1 업데이트

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.autoconfigure.
+        SpringBootApplication;
+
+//메인클래스
+@SpringBootApplication //스프링부트의 자동설정,Bean읽기,생성 모두 자동설정 (이 위치부터 읽기때문에 항상프로젝트상단에위치)
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+
+
+}
---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name); //검증하고 싶은 대상을 메소드 인자로 받음
+        assertThat(dto.getAmount()).isEqualTo(amount); // assertThat 값과 itEqualTo 값이 같을때만 성공
+    }
+}
